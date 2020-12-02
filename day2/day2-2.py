0057448803d0d1347ccc2d5d67a3d58c33bae4d9
Jan Hoppmann <jhoppmann@impossiblynamed.de>
2020-12-02 09:33:47 +0100

    Day 2 cleanup

--- day2/day2-2.py
+++ day2/day2-2.py
@@ -6,12 +6,11 @@ values = [x.split(' ', 1) for x in input_content]
 correct_passwords = 0
 
 for password_info in values:
-    positions = [int(x) for x in password_info[0].split('-')]
+    positions = [int(x)-1 for x in password_info[0].split('-')]
     character = password_info[1].split(':')[0]
     password = password_info[1].split(':')[1].strip()
 
-    print(character, password, positions, positions[0]-1, positions[1]-1)
-    if (password[positions[0]-1] == character) ^ (password[positions[1]-1] == character):
+    if (password[positions[0]] == character) ^ (password[positions[1]] == character):
         correct_passwords += 1
 
 print(correct_passwords)
